9a58a9a489060d532e689dbce93f36a702143289
berkan10 <50495877+berkan10@users.noreply.github.com>
2021-05-28 14:46:14 +0200

    Add files via upload

--- Client/main.c
+++ Client/main.c
@@ -18,8 +18,16 @@ void *subs = zmq_socket (context, ZMQ_SUB);
 
 
 
-zmq_connect (push, "tcp://benternet.pxl-ea-ict.be:24041");
-zmq_connect (subs, "tcp://benternet.pxl-ea-ict.be:24042");
+int rp = zmq_connect (push, "tcp://benternet.pxl-ea-ict.be:24041");
+int rs = zmq_connect (subs, "tcp://benternet.pxl-ea-ict.be:24042");
+sleep(1);
+
+if (rp != 0 && rs != 0)
+{
+    printf("ERROR: ZeroMQ error occurred during zmq_ctx_new(): %s\n", zmq_strerror(errno));
+
+    return EXIT_FAILURE;
+} else{printf(" Connected succesfully\n");}
 
 
 char pushtask[]     = "FIFA!<task<Berkan>"; //Is the same as the subbed TASK
@@ -29,14 +37,17 @@ char subtask[]      = "FIFA!<task<Berkan>"; //Is the same as the pushed TASK
 char subanswerpackage[] = "FIFA!<answer<package>";
 //char pushpackage[]  = ""
 
-char buffer[256];
-char buffer2[256];
-char buffer3[256];
-char buffer4[256];
+char buffer[256]="";
+char buffer2[256]= "";
+char buffer3[500]= "";
+char buffer4[256]= "";
+char buffer5[256]= "";
 
 char ask[500]="";
 char ask1[500]="";
 char ask2[500]="";
+char ask3[500]="";
+char ask4[500]="";
 
 
 
@@ -44,15 +55,10 @@ zmq_setsockopt(subs, ZMQ_SUBSCRIBE, subtask, strlen(subtask));
 zmq_setsockopt(subs, ZMQ_SUBSCRIBE, subanswer, strlen(subanswer));
 zmq_setsockopt(subs, ZMQ_SUBSCRIBE, subanswerpackage, strlen(subanswerpackage));
 
-zmq_setsockopt(subs, ZMQ_SUBSCRIBE, subanswer, strlen(subanswer));
-zmq_setsockopt(subs, ZMQ_SUBSCRIBE, subanswer, strlen(subanswer));
-
-
+// receive welcome message
 
 zmq_recv(subs,buffer,255,0);
 printf("%s", buffer);
-
-
 
 
 // reply to message go
@@ -61,34 +67,57 @@ scanf("%s",ask);
 strcat(pushanswer,ask);
 zmq_send(push, pushanswer, 15, 0);
 
-// receive welcome message
+// receive input options
 
-zmq_recv(subs,buffer2,255,0);
+zmq_recv(subs,&buffer2,255,0);
 printf("%s", buffer2);
 
-// reply pack number
+restartgame:
+// reply pack number #1
 
 scanf("%s", ask1);
 strcat(pushtask,ask1);
 zmq_send(push,pushtask,20,0);
 
+zmq_setsockopt(subs, ZMQ_UNSUBSCRIBE, subtask, strlen(subtask));
+zmq_setsockopt(subs, ZMQ_UNSUBSCRIBE, subanswer, strlen(subanswer));
 
 //receive pack results
 
-zmq_recv(subs,buffer3,25,0);
-printf("%s", buffer3);
+int result = zmq_recv(subs,&buffer3,400,0);
+//printf("%i", result);
+printf("received : %s\n", buffer3);
 
 
-scanf("%s", ask1);
+// reply pack numer #2
+scanf("%s", ask2);
 strncpy(buffer4, "FIFA!<task<Berkan>" ,22);
-strcat(buffer4,ask1);
+strcat(buffer4,ask2);
 zmq_send(push,buffer4,20,0);
-printf("%s", buffer4 );
 
 
+//receive pack results
+zmq_recv(subs,&buffer4,400,0);
+printf("received : %s\n", buffer4);
 
+// reply pack number #3
 
+scanf("%s", ask3);
+strncpy(buffer5, "FIFA!<task<Berkan>" ,22);
+strcat(buffer5,ask3);
+zmq_send(push,buffer5,20,0);
 
-zmq_close (push);
-zmq_ctx_destroy (context);
+//receive pack results
+zmq_recv(subs,&buffer5,400,0);
+printf("received : %s", buffer5);
+
+printf("\n Would u Like to play again? ( Y/N) .\n)") ;
+ scanf("%s", ask4);
+     if (strncmp("Y",ask4,1) == 0 )
+  {   goto restartgame;     }
+    else if (strncmp("N",ask4,1) == 0 )     {
+    printf("Have a nice day \n");    }
+
+//zmq_close (push);
+//zmq_ctx_destroy (context);
 }
--- MinZMQt/main.cpp
+++ MinZMQt/main.cpp
@@ -29,87 +29,103 @@ int main( int argc, char *argv[] )
 {
     //QCoreApplication a(argc, argv);
 
-    goudpack g;
-    zilverpack z;
-    bronspack b;
+           goudpack g;
+           zilverpack z;
+           bronspack b;
 
-    pakket *goudP = nullptr;
-    pakket *zilverP = nullptr;
-    pakket *bronsP = nullptr;
+           pakket *goudP = nullptr;
+           pakket *zilverP = nullptr;
+           pakket *bronsP = nullptr;
 
-    goudP = &g;
-    zilverP = &z;
-    bronsP = &b;
+           goudP = &g;
+           zilverP = &z;
+           bronsP = &b;
 
-    context = zmq_ctx_new(); // thread safe
-    void * pusher = zmq_socket( context, ZMQ_PUSH );
-    void * subs = zmq_socket( context, ZMQ_SUB );
+
+
+
+
+          context = zmq_ctx_new(); // thread safe
+          void * pusher = zmq_socket( context, ZMQ_PUSH );
+          void * subs = zmq_socket( context, ZMQ_SUB );
+
+
+
 
     // variable
-    char choise[100]="";
-    char choise_package[100] = "";
-    char s[] = "FIFA!>Berkan>Welcome to FIFA\n Type 'Go' or 'go' to get further";
-    string str("Choose your package to get your cards (press number) ");
-    char buffer[256];
-    char buffer2[256];
-    char buffer3[256];
-    char buffer4[512];
-    int gamescore = 0;
-    char Response;
-
-    zmq_connect(pusher,"tcp://benternet.pxl-ea-ict.be:24041");// zmq CONNECT SOCKET TO AN ENDPOINT AND THEN ACCEPT INCOMONG CONNECTIONS ON THAT ENDPOINT
-    zmq_connect(subs, "tcp://benternet.pxl-ea-ict.be:24042" );
-
-     char pushtask[]     = "FIFA!<task<Berkan>"; //Is the same as the subbed TASK
-     char subanswer[]    = "FIFA?<answer>";    //Is the same as the pushed ANSWER
-     char pushanswer[]   = "FIFA!<answer>";    //Is the same as the subbed ANSWER
-     char pushanswerpackage[] = "FIFA!<answer<package>";
-
-    zmq_setsockopt(subs, ZMQ_SUBSCRIBE, subanswer, strlen(subanswer));
 
 
-    printf("SERVICE STARTED SUCESSFULLY\n\n");
-    srand(time(NULL));
+              char choise_package[100] = "";
 
 
-    // message to client
-
-    strncpy(buffer, pushtask, 19);
-    strncat(buffer, "Welcome to FIFA\n Type 'Go' or 'go' to get further\n", 71);
-    zmq_send(pusher,buffer, strlen(buffer), 0);
-
-
-
-
-    // received message
-
-    zmq_recv(subs,buffer2,255,0);
+              char buffer[256]="";
+              char buffer2[256]="";
+              char buffer3[256]="";
+              char buffer4[512]="";
+              char buffer5[512]="";
+              char buffer6[512]="";
 
 
+              int stringToInt;
+              int keuze_pakket = 0;
 
 
-    //parsing van choice en het woord go
+              zmq_connect(pusher,"tcp://benternet.pxl-ea-ict.be:24041");// zmq CONNECT SOCKET TO AN ENDPOINT AND THEN ACCEPT INCOMONG CONNECTIONS ON THAT ENDPOINT
+              zmq_connect(subs, "tcp://benternet.pxl-ea-ict.be:24042" );
+
+               char pushtask[]     = "FIFA!<task<Berkan>"; //Is the same as the subbed TASK
+               char subanswer[]    = "FIFA?<answer>";    //Is the same as the pushed ANSWER
+               char pushanswer[]   = "FIFA!<answer>";    //Is the same as the subbed ANSWER
+               char pushanswerpackage[] = "FIFA!<answer<package>";
+
+               zmq_setsockopt(subs, ZMQ_SUBSCRIBE, subanswer, strlen(subanswer));
+
+
+               printf("SERVICE STARTED SUCESSFULLY\n\n");
+               srand(time(NULL));
+
+
+               // message to client
+
+
+               strncpy(buffer, pushtask, 19);
+               strncat(buffer, "Welcome to FIFA\n Type 'Go' or 'go' to get further\n", 71);
+               zmq_send(pusher,buffer, strlen(buffer), 0);
+
+
+              // received message
+
+               zmq_recv(subs,buffer2,255,0);
+
+
+
+
+        //parsing van choice en het woord go
         char* sep = strtok(buffer2,"<");
         char* sep1 = strtok(NULL,">");
         char* sep2 = strtok(NULL,">");
 
- //  printf("%s\n",sep2);
- //  printf("%s\n",sep1);
- //  printf("%s\n",sep);
 
- //   printf("%c",choise);
-
+        //  printf("%s\n",sep2);
+        //  printf("%s\n",sep1);
+        //  printf("%s\n",sep);
+        //  printf("%c",choise);
 
 
     //parsing van choice en het woord go
     if(strncmp(sep2,"go",2) ==0){
         strncpy(buffer3, pushanswer, 100);
-        strncat(buffer3, "Welcome to FIFA Ultimate Team pack opening let's gooooooo\nChoose your package to get your cards (press number)\n1 GOLD PACK \n2 SILVER PACK \n3 BRONZE PACK \n", 256);
+        strncat(buffer3, "Welcome to FIFA Ultimate Team pack opening let's gooooooo\nYou got 3 attempts\nChoose your package to get your cards (press number)\n1 GOLD PACK \n2 SILVER PACK \n3 BRONZE PACK \n", 256);
         zmq_send( pusher, buffer3, strlen(buffer3),0 );
     }
 
     zmq_setsockopt(subs, ZMQ_UNSUBSCRIBE, subanswer, strlen(subanswer));
     zmq_setsockopt(subs, ZMQ_SUBSCRIBE, pushtask, strlen(pushtask));
+
+
+
+
+
 
 
     while (1)
@@ -124,14 +140,20 @@ int main( int argc, char *argv[] )
 
 
 
-          // printf("%s", parsing2);
-             printf("%s\n",parsing1);
-           //  printf("%s\n",parsing);
+                stringToInt = atoi(parsing1);
+                keuze_pakket = stringToInt;
+
+               // printf("dit is de parsing : %i",keuze_pakket);
+
+               // printf("%s", parsing2);
+               // printf("%s\n",parsing1);
+               // printf("%s\n",parsing);
 
 
 
 
-                 if(strncmp("1",parsing1,1) == 0)
+
+                 if(keuze_pakket == 1)
                  {
                   //   std::stringstream playerGold;
                      string mes = "\nSpeler:      =>";
@@ -162,14 +184,14 @@ int main( int argc, char *argv[] )
 
 
                      strncpy(buffer4, pushanswerpackage, 22);
-                     strncat(buffer4, "mes.c_str()" ,25);
+                     strncat(buffer4,mes.c_str() ,strlen(mes.c_str()));
                      zmq_send(pusher,buffer4, strlen(buffer4), 0);
 
 
 
                  }
 
-                else if(strncmp("2",parsing1,1) == 0)
+                else if(keuze_pakket == 2)
                  {
                    //  std::stringstream playerBrons;
                     // playerBrons << b.getNaamspeler();
@@ -192,12 +214,14 @@ int main( int argc, char *argv[] )
                      mes1.append(z.getLandCoach());
                      mes1.append("\n Leeftijd       =>");
                      mes1.append(z.getLeeftijdCoach());
-                     zmq_send( pusher,mes1.c_str(), mes1.size(),0);
+                     strncpy(buffer5, pushanswerpackage, 22);
+                     strncat(buffer5,mes1.c_str() ,strlen(mes1.c_str()));
+                     zmq_send(pusher,buffer5, strlen(buffer5), 0);
 
                  }
-                 else if(strncmp("3",parsing1,1) == 0){
+                 else if(keuze_pakket == 3){
 
-                     string mes2 = "Speler:     =>";
+                     string mes2 = "\nSpeler:     =>";
                      mes2.append(b.getNaamspeler());
                      mes2.append("\n country    =>");
                      mes2.append(b.getLandSpeler());
@@ -205,8 +229,10 @@ int main( int argc, char *argv[] )
                      mes2.append(b.getRatingSpeler());
                      mes2.append("\n leeftijd   =>");
                      mes2.append(b.getLeeftijdSpeler());
+                     strncpy(buffer6, pushanswerpackage, 22);
+                     strncat(buffer6,mes2.c_str() ,strlen(mes2.c_str()));
 
-                     zmq_send( pusher,mes2.c_str(), mes2.size(),0);
+                     zmq_send(pusher,buffer6, strlen(buffer6), 0);
                  }
                  else if(strncmp("End",parsing1,1) == 0){
                     // break;
